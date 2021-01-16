a7e9b27e4a7480118f5580c8ce74627de7a24b4d
Pedro Amaral <pedromiglou@hotmail.com>
2021-01-16 01:00:05 +0000

    fixes

--- src/app/register.service.ts
+++ src/app/register.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {Observable} from 'rxjs/Internal/Observable';
+import {Observable} from 'rxjs';
 import {baseURL} from '../environments/environment';
 
 const httpOptions = {
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
   production: true
 };
+
+export const baseURL = 'http://localhost:8000/ws';
